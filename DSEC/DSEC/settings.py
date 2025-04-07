17df62a55042a9931e3809cc83f7b61c726eda86
Nagul75 <nagulk697@gmail.com>
2025-04-07 11:06:55 +0530

    remove pycache

--- DSEC/DSEC/settings.py
+++ DSEC/DSEC/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'DSEC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defsecone',
+        'USER': 'Nagul',
+        'PASSWORD': '252005',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- DSEC/base/models.py
+++ DSEC/base/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)  # Save to the correct database
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username, email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username

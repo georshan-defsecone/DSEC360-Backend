a9fb02bcbe59126ae4b9bfb31af99cb988d30b0d
Rohinth <rohinths7@gmail.com>
2025-04-08 16:34:10 +0530

    setup jwt with admin attribute

--- DSEC/base/serializers.py
+++ DSEC/base/serializers.py
@@ -5,9 +5,19 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # Add custom claims
+        # Add custom claims to token payload
         token['username'] = user.username
         token['email'] = user.email
-        token['is_admin'] = user.is_superuser
+        token['is_admin'] = user.is_admin
 
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Include additional user info in the response body
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        data['is_admin'] = self.user.is_superuser
+
+        return data

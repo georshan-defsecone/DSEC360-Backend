eb9bdfad611d9bb94b8fd24a798e20e1af865cb9
Nagul75 <nagulk697@gmail.com>
2025-04-09 12:41:50 +0530

    commit ignore

--- DSEC/DSEC/settings.py
+++ DSEC/DSEC/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'base.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

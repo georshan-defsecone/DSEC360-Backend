68f9d5df248aabd943a3691566414075f3548639
Nagul75 <nagulk697@gmail.com>
2025-04-11 11:25:22 +0530

    Add compliance urls

--- DSEC/base/urls.py
+++ DSEC/base/urls.py
@@ -1,17 +1,18 @@
 # base/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import get_all_users
-from .views import MyProjectsView, add_MyProjectsView, trashed_scans_view, update_MyProjectsView, compliance_data
+from . import views
+
 
 urlpatterns = [
-    path('scans/', MyProjectsView.as_view(), name='scan-list'),
-    path('scans/add/', add_MyProjectsView, name='scan-add'),
-    path('scans/<str:scan_id>/trash/', update_MyProjectsView, name='move_to_trash'),
-    path('scans/trashed/', trashed_scans_view, name='trashed_scans'),
-    path('users/', get_all_users, name='get-all-users'),
-    path('compliance/<str:os_name>/', compliance_data, name='compliance-data'),
+    path('scans/', views.MyProjectsView.as_view(), name='scan-list'),
+    path('scans/add/', views.add_MyProjectsView, name='scan-add'),
+    path('scans/<str:scan_id>/trash/', views.update_MyProjectsView, name='move_to_trash'),
+    path('scans/trashed/', views.trashed_scans_view, name='trashed_scans'),
+    path('users/', views.get_all_users, name='get-all-users'),
+    path('compliance/configaudit/<str:os_name>/', views.get_compliance_data, name='compliance-data'),
+    path('compliance/ioc/<str:os_name>/', views.get_compromise_assessment_data, name='compromise-assessment-data'),
 
 ]
 
